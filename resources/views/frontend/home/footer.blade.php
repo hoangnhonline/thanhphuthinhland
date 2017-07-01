d2857bce90f5b8d5e1d15a5415d7a9fe3e5b1925
HoangNH <hoangnhonline@gmail.com>
2017-07-01 12:44:42 +0700

    update maps

--- resources/views/frontend/home/footer.blade.php
+++ resources/views/frontend/home/footer.blade.php
@@ -47,12 +47,5 @@ hs.src = ('//s10.histats.com/js15_as.js');
 <!-- Histats.com  END  -->
             </div>
     	</div>
-    </div>
-    <!--<div class="block-info-bottom">
-    	<div class="container">
-    		<p><strong>CÔNG TY CỔ PHẦN MUA BÁN</strong>. MST: 0102344810, cấp ngày 10/08/2007. SĐT: 08-73001234</p>
-    		<p><strong>Trụ sở chính</strong> Số 28-30 Đường số 2, khu phố Hưng Gia 5, phường Tân Phong, Quận 7, TP. Hồ Chí Minh</p>
-    		<p>muaban.net có trách nhiệm chuyển tải thông tin. Không chịu bất kỳ trách nhiệm nào từ các tin này. <a href="#" title="">xem chi tiết</a></p>
-    	</div>
-    </div>-->
+    </div> 
 </footer><!-- /footer -->
