cd8e2b1385a4ac9cec6103433da8a7bbb97b95b1
HoangNH <hoangnhonline@gmail.com>
2017-10-18 15:42:01 +0700

    .

--- app/Http/Controllers/Backend/UserWorkController.php
+++ app/Http/Controllers/Backend/UserWorkController.php
@@ -46,6 +46,7 @@ class UserWorkController extends Controller
         if(Auth::user()->role == 1 ){
             $query->where('user_work.created_user', Auth::user()->id);
         }elseif(Auth::user()->role == 2){
+            $userIdArr = [];
             $userList = Account::where('role', 1)->join('user_mod', 'user_mod.user_id', '=', 'users.id')
                     ->where('user_mod.mod_id', Auth::user()->id)->get();
             if($userList){
