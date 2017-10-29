0d6d4959463c50672c07c7f8342d34eadf891683
HoangNH <hoangnhonline@gmail.com>
2017-10-29 21:27:29 +0700

    .

--- resources/views/backend/cate/index.blade.php
+++ resources/views/backend/cate/index.blade.php
@@ -81,10 +81,10 @@
                                 <tr id="row-{{ $item->id }}">
                                     <td><span class="order">{{ $i }}</span></td>
                                     <td style="vertical-align:middle;text-align:center">
-                                        <img src="{{ URL::asset('public/admin/dist/img/move.png')}}" class="move img-thumbnail" alt="Cập nhật thứ tự"/>
+                                        <img src="{{ URL::asset('backend/dist/img/move.png')}}" class="move img-thumbnail" alt="Cập nhật thứ tự"/>
                                     </td>
                                     <td>
-                                      <img class="img-thumbnail lazy" width="100" data-original="{{ $item->image_url ? Helper::showImage($item->image_url) : URL::asset('public/admin/dist/img/no-image.jpg') }}" alt="Nổi bật" title="Nổi bật" />
+                                      <img class="img-thumbnail lazy" width="100" data-original="{{ $item->image_url ? Helper::showImage($item->image_url) : URL::asset('backend/dist/img/no-image.jpg') }}" alt="Nổi bật" title="Nổi bật" />
                                     </td>
                                     <td>
                                         <a href="{{ route( 'cate.edit', [ 'id' => $item->id ]) }}">{{ $item->name }}</a>
